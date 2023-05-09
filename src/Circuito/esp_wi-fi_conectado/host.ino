5fe5eead3bbf76f3c46fb2b0e350361f3ea4a317
Txrmarcos <110488788+Txrmarcos@users.noreply.github.com>
2023-05-09 16:16:24 -0300

    Add files via upload

---
+++ src/Circuito/esp_wi-fi_conectado/host.ino
@@ -0,0 +1,24 @@
+#include <WiFi.h>
+
+const char* ssid = "Inteli-COLLEGE";
+const char* password = "QazWsx@123";
+
+void setup() {
+  pinMode(4, OUTPUT);
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(4, HIGH);
+    delay(1000);
+    Serial.println("Conectando ao WiFi...");
+  }
+
+  Serial.println("Conectado ao WiFi!");
+  Serial.print("Endereço IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // Seu código aqui
+}
