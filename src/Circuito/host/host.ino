335ac88d50ac25089808bb7f0b459c769e9ac9bf
Thomaz-Klifson <110629744+Thomaz-Klifson@users.noreply.github.com>
2023-05-11 16:49:46 -0300

    Atualização Marcos 11/05

--- src/Circuito/host/host.ino
+++ src/Circuito/host/host.ino
@@ -3,116 +3,74 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// Define o endereço do display LCD (verificar com o datasheet do módulo I2C)
 #define LCD_ADDRESS 0x27
-
-// Define o número de colunas e linhas do display LCD
 #define LCD_COLUMNS 16
 #define LCD_ROWS 2
 
-// Cria um objeto LiquidCrystal_I2C para controlar o display LCD
 LiquidCrystal_I2C lcd(LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS);
 
-
-//Nome e Senha da rede WIFI
 const char* ssid = "Inteli-COLLEGE";
 const char* password = "QazWsx@123";
+String receivedMac = "";
 
-//Inicia Server na porta 80
 WiFiServer server(80);
-
-//Cria uma variavel client
 WiFiClient client;
 
-//Lista de endereços MAC
-String salas[5] = {"FC:5C:45:00:4F:C8","FC:5C:45:00:4D:88","FC:5C:45:00:60:98","FC:5C:45:00:5C:F8","FC:5C:45:00:57:08"};
-String lista[20]={};
-
-//dado nome aos LEDS
 const int naoConectadoLedVermelho = 4;
 const int conectadoLedVerde = 0;
 const int conectandoLedAmarelo = 19;
 
-int i; 
-
-void LCD(){
-  // Inicializa a comunicação I2C com o display LCD
+void LCD() {
   Wire.begin();
-  // Inicializa o display LCD
   lcd.init();
-  // Liga o backlight do display LCD
   lcd.backlight();
 }
 
-
-
-void connectWifi(){
-  //Se o ESP32 não conectou no WIFI ele entra no loop e gera a mensagem de "Conectando ao WiFi..."
+void connectWifi() {
   while (WiFi.status() != WL_CONNECTED) {
-  digitalWrite(naoConectadoLedVermelho, HIGH);
-  delay(1000);
-  Serial.println("Conectando ao WiFi...");
+    digitalWrite(naoConectadoLedVermelho, HIGH);
+    delay(5000);
+    Serial.println("Conectando ao WiFi...");
   }
 }
 
-void infoNet(){
-  //Quando o ESP32 se conecta no WIFI ele retorna no Serial que está conectado a rede e o IpLocal.
+void infoNet() {
   Serial.println("Conectado ao WiFi!");
   Serial.print("Endereço IP: ");
   Serial.println(WiFi.localIP());
-
 }
 
-void iniciaServer(){
-  //inicia o server na porta 80
+void iniciaServer() {
   server.begin();
   Serial.println("Servidor iniciado na porta 80.");
 }
 
-void verificaCliente(){
-  // Aguarda conexão de um cliente (outro ESP32) e enquanto não conecta ele pisca o led amarelo na protoboard
-  if (!client || !client.connected()) {
+void verificaCliente() {
+  if (!client.connected()) {
     client = server.available();
     digitalWrite(conectandoLedAmarelo, HIGH);
     delay(1000);
     digitalWrite(conectandoLedAmarelo, LOW);
     delay(1000);
-
     return;
   }
 }
 
-void mensagemClient(){
-  //Verifica se o client(ESP32) mandou alguma mensagem.  
+void mensagemCliente(String receivedMac) {
   if (client.available()) {
 
-  // Lê a mensagem enviada pelo cliente
-  String mensagem = client.readStringUntil('\n');
-
-  //retorna uma nova string sem os espaços em branco.
-  mensagem.trim();
-
-  // Imprime a mensagem recebida do client.
-  Serial.print("Mensagem recebida: ");
-  Serial.println(mensagem);
-
-  //Da um delay de 0.5segundos
-  delay(500);
-
-  lcd.setCursor(0, 0);
-  lcd.print(mensagem);
-  // Posiciona o cursor na segunda linha, primeira coluna e imprime " "
-  delay(5000);
-  lcd.setCursor(0, 1);
-  lcd.print(mensagem);
-
+    String receivedMac = client.readStringUntil('\n');
+    receivedMac.trim();
+    Serial.print("Mensagem recebida: ");
+    Serial.println(receivedMac);
+    lcd.setCursor(0, 0);
+    lcd.print(receivedMac);
+    lcd.setCursor(0, 1);
+    lcd.print(receivedMac);
+  }
 }
 
-
-
-}
-
-void alertaConexao(int led, char * message) {
+void alertaConexao(int led, char* message) {
   while (true) {
     Serial.println(message);
     digitalWrite(led, HIGH);
@@ -122,51 +80,22 @@ void alertaConexao(int led, char * message) {
   }
 }
 
-
 void setup() {
-  //Porta de saida para as informações(Porta Serial)
-  Serial.begin(9600); 
-
-  //definição dos LEDS 
+  Serial.begin(9600);
   pinMode(naoConectadoLedVermelho, OUTPUT);
   pinMode(conectandoLedAmarelo, OUTPUT);
   pinMode(conectadoLedVerde, OUTPUT);
-
-  //Conecta com o Wifi pegando o Nome e a Senha da rede
   WiFi.begin(ssid, password);
-
-  //Chama a função connectWifi. 
   connectWifi();
-
-  //Desliga o Led vermelho assim que conecta ao Wifi
   digitalWrite(naoConectadoLedVermelho, LOW);
-
-  //Chama a função infoNet.
   infoNet();
-
-  //Chama a função iniciaServer.
   iniciaServer();
-
-  //Chama a função LCD
   LCD();
-
 }
-
 
 void loop() {
-
-  //Liga o Led Verde
-  digitalWrite(conectadoLedVerde, HIGH); 
-  //Chama a função verificaCliente.
+  digitalWrite(conectadoLedVerde, HIGH);
   verificaCliente();
-  //Chama a função mensagemCliente.
-  mensagemClient();
-  //Liga o Led amarelo
+  mensagemCliente(receivedMac);
   digitalWrite(conectandoLedAmarelo, HIGH);
-
 }
-
-  
-
-
-
