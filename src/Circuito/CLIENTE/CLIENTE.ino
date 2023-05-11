1a1b5592dda0582485e44677d3590abf5e0b309d
Txrmarcos <marcos.teixeira@sou.inteli.edu.br>
2023-05-11 02:27:57 -0300

    marcos 0.7

--- src/Circuito/CLIENTE/CLIENTE.ino
+++ src/Circuito/CLIENTE/CLIENTE.ino
@@ -1,62 +1,138 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 
-const char* ssid = "Inteli-COLLEGE";
-const char* password = "QazWsx@123";
-const char* serverAddress = "10.128.69.94"; // Endereço IP local do servidor
+const char* ssid = "iPhone";
+const char* password = "12376528";
+const char* serverAddress = "10.254.18.212"; // Endereço IP local do servidor
 int serverPort = 80; // Porta usada pelo servidor
 WiFiClient client;
+  int cont = 0;
 
+// Número máximo de endereços MAC que podem ser armazenados
+const int MAX_MACS = 5;
 
-void setup() {
-  Serial.begin(9600);
+String macs[MAX_MACS];
 
-  WiFi.begin(ssid, password);
+int numMacs = 0;
 
-  while (WiFi.status() != WL_CONNECTED) {
+void wifiWait(){
+  //Entra num loop se o dispositivo (ESP32) não conecta a rede.
+    while (WiFi.status() != WL_CONNECTED) {
     Serial.println("Conectando ao WiFi...");
     delay(1000);
   }
+}
 
-
+void infoRede(){
+  //Retorna no Serial que o disponitivo esta conectado e o Indereço IP Local
   Serial.println("Conectado ao WiFi!");
   Serial.print("Endereço IP: ");
   Serial.println(WiFi.localIP());
 
   String bssidStr = WiFi.BSSIDstr(); // pega o endereço mac em tempo real
-  Serial.print("Endereço MAC do roteador: "); //print
-  Serial.println(bssidStr);//print
+  Serial.print("Endereço MAC do roteador: "); //printa o endereço MAC
+  Serial.println(bssidStr);
 
+}
 
-    // Conecta ao servidor
-  while (!client.connect(serverAddress, 80)) { // Substitua pelo endereço IP do host
+void serverConect(){
+
+  // Se o client não conecta no ip e na porta ele entra num loop que volta no Serial a mensagem : "Falha na conexão com o server."
+  while (!client.connect(serverAddress, 80)) {
     Serial.println("Falha na conexão com o server.");
     delay(1000);
   }
+}
 
+void returnMac(){
+  String bssidStr = WiFi.BSSIDstr(); //pega a string do endereço mac
+
+  //Salva o endereço MAC na varial mensagem
+
+  String mensagem = bssidStr; // acho q o erro ta aqui, o bssidStr não está atualizando quando eu uso o replace
+
+  //Verifica se o outro dispositivo mandou mensagem.
+  client.available();
+
+  // Envia o Endereço MAC para o servidor
+  client.println(mensagem);
+  Serial.println(mensagem); // printa no Serial 
+
+}
+
+void identificaLocal(){
+   // Verifica se a mensagem é um endereço MAC válido
+    if (mensagem.length() == 12) {
+      // Verifica se o endereço MAC já está na lista
+      bool encontrado = false;
+      for (int i = 0; i < numMacs; i++) {
+        if (mensagem == macs[i]) {
+          encontrado = true;
+          break;
+        }
+      }
+
+      if (!encontrado) {
+        // Adiciona o endereço MAC à lista
+        if (numMacs < MAX_MACS) {
+          macs[numMacs] = mensagem;
+          client.println(macs[numMacs]); // joga o para o serial do client 
+          numMacs++;
+          delay(500);
+          Serial.println("Endereço MAC adicionado à lista.");
+        } else {
+          Serial.println("A lista de endereços MAC está cheia.");
+        }
+      } else {
+        Serial.println("Endereço MAC já existe na lista.");
+      }
+    } else {
+      Serial.println("Endereço MAC inválido.");
+    }
  
-
-
+    if(mensagem == macs[cont]){
+      Serial.println("marcos ta no 1");
+    } 
+    cont++;
+    if(mensagem == macs[cont]){
+      Serial.println("marcos ta no 2");
+    } 
+    cont++;
+    if(mensagem == macs[cont]){
+      Serial.println("marcos ta no 3");
+    } 
+    cont++;
+    if(mensagem == macs[cont]){
+      Serial.println("marcos ta no 4");
+    }
+    cont= 0;
 
 }
 
 
-void loop() {
+void setup() {
+  //define a porta do serial que mostrara as informarções
+  Serial.begin(9600);
 
+  WiFi.begin(ssid, password);
+  //Chama a função wifiWait
+  wifiWait();
+  //Chama a função infoRede
+  infoRede();
+  //Chama a função serverConect
+  serverConect();
+}
+
+
+void loop() {
+  //Mostra no serial que esta enviando a mensagem para o server.
   Serial.println("Enviando Mensagem ao Server:");
 
-
-  String bssidStr = WiFi.BSSIDstr(); //pega o endereço mac
-
-
-  String mensagem = bssidStr; // acho q o erro ta aqui, o bssidStr não está atualizando quando eu uso o replace
-  mensagem.replace(":", "");//tira os ":" do Mac Adress
-
-  client.available();
-    // Envia mensagem para o servidor
-
-  client.println(mensagem); // joga o para o serial do client 
-  Serial.println(mensagem); // printa no Serial 
-  delay(500);
+  //Chama a função returnMac
+  returnMac();
+  //Chama a função identificaLocal
+  identificaLocal()
+  
+  
 
 }
--- src/Circuito/host/host.ino
+++ src/Circuito/host/host.ino
@@ -1,79 +1,113 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
-const char* ssid = "Inteli-COLLEGE";
-const char* password = "QazWsx@123";
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// Define o endereço do display LCD (verificar com o datasheet do módulo I2C)
+#define LCD_ADDRESS 0x27
+
+// Define o número de colunas e linhas do display LCD
+#define LCD_COLUMNS 16
+#define LCD_ROWS 2
+
+// Cria um objeto LiquidCrystal_I2C para controlar o display LCD
+LiquidCrystal_I2C lcd(LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS);
 
 
+//Nome e Senha da rede WIFI
+const char* ssid = "Thomaz Klifson";
+const char* password = "inteli10";
+
+//Inicia Server na porta 80
+WiFiServer server(80);
+
+//Cria uma variavel client
+WiFiClient client;
+
+//Lista de endereços MAC
+String salas[5] = {"FC:5C:45:00:4F:C8","FC:5C:45:00:4D:88","FC:5C:45:00:60:98","FC:5C:45:00:5C:F8","FC:5C:45:00:57:08"};
+String lista[20]={};
+
+//dado nome aos LEDS
 const int naoConectadoLedVermelho = 4;
 const int conectadoLedVerde = 0;
 const int conectandoLedAmarelo = 19;
 
+int i; 
 
-WiFiServer server(80);
-WiFiClient client;
-int i;
-String salas[5] =  { 
-  "FC:5C:45:00:4F:C8", 
-  "FC:5C:45:00:4D:88", 
-  "FC:5C:45:00:60:98", 
-  "FC:5C:45:00:5C:F8", 
-  "FC:5C:45:00:57:08" 
-};
+void LCD(){
+  // Inicializa a comunicação I2C com o display LCD
+  Wire.begin();
+  // Inicializa o display LCD
+  lcd.init();
+  // Liga o backlight do display LCD
+  lcd.backlight();
+}
 
-void setup() {
-  pinMode(naoConectadoLedVermelho, OUTPUT);
-  pinMode(conectandoLedAmarelo, OUTPUT);
-  pinMode(conectadoLedVerde, OUTPUT);
-  Serial.begin(9600);
+void textoLcd(){
+  lcd.setCursor(0, 0);
+  lcd.print(mensagem);
+  // Posiciona o cursor na segunda linha, primeira coluna e imprime " "
+  delay(5000)
+  lcd.setCursor(0, 1);
+  lcd.print(mensagem);
+}
 
-  WiFi.begin(ssid, password);
+void connectWifi(){
+  //Se o ESP32 não conectou no WIFI ele entra no loop e gera a mensagem de "Conectando ao WiFi..."
   while (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(4, HIGH);
-    delay(1000);
-    Serial.println("Conectando ao WiFi...");
+  digitalWrite(naoConectadoLedVermelho, HIGH);
+  delay(1000);
+  Serial.println("Conectando ao WiFi...");
   }
-  Serial.println("Conectado ao WiFi!");
+}
+
+void infoNet(){
+  //Quando o ESP32 se conecta no WIFI ele retorna no Serial que está conectado a rede e o IpLocal.
+/  Serial.println("Conectado ao WiFi!");
   Serial.print("Endereço IP: ");
   Serial.println(WiFi.localIP());
-  server.begin();  //inicia o server no  server port 80
+
+}
+
+void iniciaServer(){
+  //inicia o server na porta 80
+  server.begin();
   Serial.println("Servidor iniciado na porta 80.");
 }
-void loop() {
-  // Aguarda conexão de um cliente
+
+void verificaCliente(){
+  // Aguarda conexão de um cliente (outro ESP32) e enquanto não conecta ele pisca o led amarelo na protoboard
   if (!client || !client.connected()) {
     client = server.available();
+    digitalWrite(conectandoLedAmarelo, HIGH);
+    delay(1000);
+    digitalWrite(conectandoLedAmarelo, LOW);
+    delay(1000);
+
     return;
   }
-  // Verifica se o cliente enviou uma mensagem
+}
+
+void mensagemClient(){
+  //Verifica se o client(ESP32) mandou alguma mensagem.  
   if (client.available()) {
-    // Lê a mensagem enviada pelo cliente
-    String mensagem = client.readStringUntil('\n');
-    mensagem.trim();
-    // Imprime a mensagem recebida
-    Serial.print("Mensagem recebida: ");
-    Serial.println(mensagem);
-    delay(500);
-    salas[0].replace(":", "");
-    if (mensagem == salas[0]) {
-      Serial.println("o Marcos está no atelie: 5 ");
-      alertaConexao(conectadoLedVerde, "Dentro da área central");
-    } else if (mensagem == salas[1]) {
-      Serial.println("o Marcos está na escadaria ");
-      alertaConexao(naoConectadoLedVermelho, "Fora da área central");
-    } else if (mensagem == salas[2]) {
-      Serial.println("o Marcos está no laboratorio ");
-      alertaConexao(naoConectadoLedVermelho, "Fora da área central");
-    } else if (mensagem == salas[3]) {
-      Serial.println("o Marcos está no canto do 3 andar ");
-      alertaConexao(naoConectadoLedVermelho, "Fora da área central");
-    } else if (mensagem == salas[4]) {
-      Serial.println("o Marcos está no 1 ");
-      alertaConexao(naoConectadoLedVermelho, "Fora da área central");
-    } else {
-      Serial.println("Sumiu");
-      alertaConexao(conectandoLedAmarelo, "Fora da área de cobertura");
-    }
-  }
+
+  // Lê a mensagem enviada pelo cliente
+  String mensagem = client.readStringUntil('\n');
+
+  //retorna uma nova string sem os espaços em branco.
+  mensagem.trim();
+
+  // Imprime a mensagem recebida do client.
+  Serial.print("Mensagem recebida: ");
+  Serial.println(mensagem);
+
+  //Da um delay de 0.5segundos
+  delay(500);
+
+}
+
 }
 
 void alertaConexao(int led, char * message) {
@@ -85,3 +119,54 @@ void alertaConexao(int led, char * message) {
     delay(300);
   }
 }
+
+
+void setup() {
+  //Porta de saida para as informações(Porta Serial)
+  Serial.begin(9600); 
+
+  //definição dos LEDS 
+  pinMode(naoConectadoLedVermelho, OUTPUT);
+  pinMode(conectandoLedAmarelo, OUTPUT);
+  pinMode(conectadoLedVerde, OUTPUT);
+
+  //Conecta com o Wifi pegando o Nome e a Senha da rede
+  WiFi.begin(ssid, password);
+
+  //Chama a função connectWifi. 
+  connectWifi();
+
+  //Desliga o Led vermelho assim que conecta ao Wifi
+  digitalWrite(naoConectadoLedVermelho, LOW);
+
+  //Chama a função infoNet.
+  infoNet();
+
+  //Chama a função iniciaServer.
+  iniciaServer();
+
+  //Chama a função LCD
+  LCD();
+
+}
+
+
+void loop() {
+
+  //Liga o Led Verde
+  digitalWrite(conectadoLedVerde, HIGH); 
+  //Chama a função verificaCliente.
+  verificaCliente();
+  //Chama a função mensagemCliente.
+  mensagemClient();
+  //Chama a função textoLcd.
+  textoLcd();
+  //Liga o Led amarelo
+  digitalWrite(conectandoLedAmarelo, HIGH);
+
+}
+
+  
+
+
+
