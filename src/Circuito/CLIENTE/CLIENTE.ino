ff514f40fd2a983ef214091d69559d72e4408ccc
Txrmarcos <134190163+MarcosTeixeira0@users.noreply.github.com>
2023-05-24 14:54:37 -0300

    subindo

--- src/Circuito/CLIENTE/CLIENTE.ino
+++ src/Circuito/CLIENTE/CLIENTE.ino
@@ -1,28 +1,18 @@
-//Chama as bibliotecas
 #include <WiFi.h>
 #include <WiFiClient.h>
 
-//Salva o nome a rede e o IP em constantes.
 const char* ssid = "Inteli-COLLEGE";
 const char* password = "QazWsx@123";
-const char* serverAddress = "10.128.66.252"; // Endereço IP local do servidor
-
-//Salva na variavel serverPort a porta 80 do servidor
-int serverPort = 443; // Porta usada pelo servidor
-
-//Salva Wifi Client em client.
+const char* serverAddress = "10.128.69.113"; // Endereço IP local do servidor
+int serverPort = 80; // Porta usada pelo servidor
 WiFiClient client;
-
-//Salva 0 na variavel cont
-int cont = 0;
-
+  int cont = 0;
 
 // Número máximo de endereços MAC que podem ser armazenados
 const int MAX_MACS = 5;
 
 String macs[MAX_MACS];
 
-//Salva 0 na variavel numMacs
 int numMacs = 0;
 
 void wifiWait(){
@@ -48,34 +38,42 @@ void infoRede(){
 void serverConect(){
 
   // Se o client não conecta no ip e na porta ele entra num loop que volta no Serial a mensagem : "Falha na conexão com o server."
-  while (!client.connect(serverAddress, serverPort)) {
+  while (!client.connect(serverAddress, 80)) {
     Serial.println("Falha na conexão com o server.");
-    delay(500);
+    delay(1000);
   }
 }
 
-/*String returnMac(){
-
+void returnMac(){
   String bssidStr = WiFi.BSSIDstr(); //pega a string do endereço mac
 
   //Salva o endereço MAC na varial mensagem
+
   String mensagem = bssidStr; // acho q o erro ta aqui, o bssidStr não está atualizando quando eu uso o replace
-  
-  mensagem.replace(":","");
+
+  //Verifica se o outro dispositivo mandou mensagem.
+  client.available();
+
+  // Envia o Endereço MAC para o servidor
+  client.println(mensagem);
   Serial.println(mensagem); // printa no Serial 
 
-  return mensagem;
   
 
-}*/
+}
 
-//void digitarMsn(){
-  
+void digitarMsn(){
+  Serial.println("digite uma mensagem:");
+  while(Serial.available() == 0){
 
+  }
 
-//}
+  String digita = Serial.readStringUntil('\n');
 
-/*void identificaLocal(String mensagem){
+  client.println(digita);
+}
+
+/*void identificaLocal(){
    // Verifica se a mensagem é um endereço MAC válido
     if (mensagem.length() == 12) {
       // Verifica se o endereço MAC já está na lista
@@ -91,6 +89,7 @@ void serverConect(){
         // Adiciona o endereço MAC à lista
         if (numMacs < MAX_MACS) {
           macs[numMacs] = mensagem;
+          client.println(macs[numMacs]); // joga o para o serial do client 
           numMacs++;
           delay(500);
           Serial.println("Endereço MAC adicionado à lista.");
@@ -104,33 +103,25 @@ void serverConect(){
       Serial.println("Endereço MAC inválido.");
     }
  
-    //Verifica se o outro dispositivo mandou mensagem.
-    
     if(mensagem == macs[cont]){
-    Serial.println("ESP32 na SALA");
-    client.println("ESP32 na SALA");
+      Serial.println("marcos ta no 1");
     } 
     cont++;
     if(mensagem == macs[cont]){
-      Serial.println("ESP32 = OB");
-      client.println("ESP32 = OB");
+      Serial.println("marcos ta no 2");
     } 
     cont++;
     if(mensagem == macs[cont]){
-      Serial.println("ESP32 = ANDAR 3");
-      client.println("ESP32 = ANDAR 3");
+      Serial.println("marcos ta no 3");
     } 
     cont++;
     if(mensagem == macs[cont]){
-      Serial.println("ESP32 = ANDAR 2");
-      client.println("ESP32 = ANDAR 2");
+      Serial.println("marcos ta no 4");
     }
     cont= 0;
 
-    // Envia o Endereço MAC para o servidor
-
-}*/
-
+}
+*/
 
 void setup() {
   //define a porta do serial que mostrara as informarções
@@ -147,33 +138,16 @@ void setup() {
 
 
 void loop() {
-  //Mostra no serial que esta enviando a mensagem para o server.
+  //Mostra no serial que está enviando a mensagem para o servidor.
   Serial.println("Enviando Mensagem ao Server:");
 
-  //Chama a função returnMac
-  //Chama a função identificaLocal
- // returnMac();
-  //delay(1000);
-  //identificaLocal(returnMac());
+  // Verifica se há dados disponíveis no Serial
+  if (Serial.available() > 0) {
+    // Lê a mensagem digitada
+    String mensagem = Serial.readStringUntil('\n');
 
-
- // Serial.println("digite uma mensagem:");
-  //if (Serial.available() >0 ) {
-  //  String digita = Serial.readStringUntil('\n');
-   // client.println(digita + '\n');
- // }
-  
-  //Manda para o client que o "ESP está na sala".
-  //client.println("ESP em sala");
-
-  //Salva o endereço MAC na variavel mac;
-  String mac = WiFi.BSSIDstr();
-  //Pega o endereço MAC e remove os pontos.
-  mac.replace(":","");
-  //printa o endereço MAC no Serial
-  Serial.println(mac);
-  client.println("ESP32 = OB");
-  Serial.println("...");
-  //Verifica se o cliente desconectou e se desconectou printa no Serial "eita saiu" 
-  delay(1000);
+    // Envia a mensagem para o servidor
+    client.println(mensagem);
+    Serial.println(mensagem);
+  }
 }
--- src/Circuito/host/host.ino
+++ src/Circuito/host/host.ino
@@ -1,163 +1,118 @@
-//Incluindo bibliotecas
-
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#include <PubSubClient.h>
-#include "UbidotsEsp32Mqtt.h"
 
-//Incluindo constantes
+// Define o endereço do display LCD (verificar com o datasheet do módulo I2C)
 #define LCD_ADDRESS 0x27
-#define LCD_COLUMNS 14
-#define LCD_ROWS 12
-#define velocidade_som 0.034
-#define polegadas 0.40
 
-//Definindo o endereço, o numero de colunas, e o numero de linhas do LCD.
+// Define o número de colunas e linhas do display LCD
+#define LCD_COLUMNS 16
+#define LCD_ROWS 2
+
+// Cria um objeto LiquidCrystal_I2C para controlar o display LCD
 LiquidCrystal_I2C lcd(LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS);
 
-//Definindo o nome e a senha da rede.
+
+//Nome e Senha da rede WIFI
 const char* ssid = "Inteli-COLLEGE";
 const char* password = "QazWsx@123";
-const char* UBIDOTS_TOKEN = "BBFF-BBFF-xWXqB1mRuV2we8DAZStWtIPrPMNdcc";
-const char* DEVICE_LABEL = "KLIF";
-const char* VARIABLE_LABEL = "esp-quebrado";
-const int PUBLISH_FREQUENCY = 1000; // Update rate in milliseconds
-unsigned long timer;
 
-//Definindo o server (server80)
-WiFiServer server(443);
+//Inicia Server na porta 80
+WiFiServer server(80);
 
-// conexão com o ubidots via token
-Ubidots ubidots(UBIDOTS_TOKEN);
-
-//Definindo WifiClient na variavel client.
+//Cria uma variavel client
 WiFiClient client;
 
-//Salvando os pinos dos leds em variaveis respectivas.
-const int naoConectadoLedVermelho = 15;
+//Lista de endereços MAC
+String salas[5] = {"FC:5C:45:00:4F:C8","FC:5C:45:00:4D:88","FC:5C:45:00:60:98","FC:5C:45:00:5C:F8","FC:5C:45:00:57:08"};
+String lista[20]={};
+
+//dado nome aos LEDS
+const int naoConectadoLedVermelho = 4;
 const int conectadoLedVerde = 0;
-const int conectandoLedAmarelo = 2;
+const int conectandoLedAmarelo = 19;
 
-long duracao;
-float distancCm;
-float distanciaInch;
-const int pinoBuzzer = 25;
-const int pinoTrig = 26;
-const int pinoEcho = 27;
+int i; 
 
-
-
-void quebrou() {
-  digitalWrite(pinoTrig, LOW);
-  delayMicroseconds(2);
-  digitalWrite(pinoTrig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(pinoTrig, LOW);
-
-  duracao = pulseIn(pinoEcho, HIGH);  // Calcule a distância
-
-  distancCm = duracao * velocidade_som / 2;  // Converter para centimetros
-  distanciaInch = distancCm * polegadas;     // Converter para polegadas
-
-  if (distancCm > 5) {
-    digitalWrite(pinoBuzzer, HIGH);
-    Serial.print("ESP32 retirado do dispositivo!!!");
-    delay(2000);
-    digitalWrite(pinoBuzzer, LOW);
-  }
-
-  Serial.print("Distancia (cm): ");  // Imprime a distância no Serial Monitor
-  Serial.println(distancCm);
-  Serial.print("Distancia (inch): ");
-  Serial.println(distanciaInch);
-  delay(1000);
-}
-
-void LCD() {
+void LCD(){
+  // Inicializa a comunicação I2C com o display LCD
   Wire.begin();
+  // Inicializa o display LCD
   lcd.init();
+  // Liga o backlight do display LCD
   lcd.backlight();
 }
 
-void connectWifi() {
-  //Enquanto não se conecta no Wifi entra num loop que liga o LED vermelho eretorna uma mensagem "conectando ao WIFI...".
-  while (WiFi.status() != WL_CONNECTED) {
 
-    digitalWrite(naoConectadoLedVermelho, HIGH);
-    delay(2000);
-    Serial.println("Conectando ao WiFi...");
+
+void connectWifi(){
+  //Se o ESP32 não conectou no WIFI ele entra no loop e gera a mensagem de "Conectando ao WiFi..."
+  while (WiFi.status() != WL_CONNECTED) {
+  digitalWrite(naoConectadoLedVermelho, HIGH);
+  delay(1000);
+  Serial.println("Conectando ao WiFi...");
   }
 }
 
-void infoNet() {
-  //Retorna no Serial que conectou no Wifi e o Endereço LocalIP conectado.
+void infoNet(){
+  //Quando o ESP32 se conecta no WIFI ele retorna no Serial que está conectado a rede e o IpLocal.
   Serial.println("Conectado ao WiFi!");
   Serial.print("Endereço IP: ");
   Serial.println(WiFi.localIP());
+
 }
 
-void iniciaServer() {
-  //Inicia o Server na porta 80.
+void iniciaServer(){
+  //inicia o server na porta 80
   server.begin();
-  //Printa no Serial "Servidor iniciado na porta 80."
   Serial.println("Servidor iniciado na porta 80.");
 }
 
-
-
-
-void verificaCliente() {
-  //Se o cliente não está conectado o Led amarelo fica piscando até o client(Outro ESP32).
-  if (!client.connected()) {
+void verificaCliente(){
+  // Aguarda conexão de um cliente (outro ESP32) e enquanto não conecta ele pisca o led amarelo na protoboard
+  if (!client || !client.connected()) {
+    client = server.available();
     digitalWrite(conectandoLedAmarelo, HIGH);
     delay(1000);
     digitalWrite(conectandoLedAmarelo, LOW);
     delay(1000);
-    Serial.println("Aguardando um Cliente");
+
     return;
   }
 }
 
-void mensagemCliente() {
-  //Verifica se o client está conectado.
-  
-    //Verifica se o client mandou mensagem.
-    if (client.available()) {
-      //Limpa as informações do Display LCD
-      lcd.clear();
-      //Pega a mensagem que o client manda e quarda na varial "mac".
-      String mac = client.readStringUntil('\n');
-      //Exclui qualquer espaço "vazio" que exista na variavel "mac"
-      mac.trim();
-      //Printa no serial a Mensagem recibida do client.
-      Serial.print("Mensagem recebida: ");
-      Serial.println(mac);
-      //Mostra na primeira linha da tela LCD a mensagem recebida do client
-      lcd.setCursor(0, 0);
-      lcd.print(mac);
-      //Mostra na segunda linha da tela LCD a mensagem recebida do client
-      lcd.setCursor(0, 1);
-      lcd.print("MAC:FC5C45004FC8");
-      //Da um delay de 1.5 segundos e limpa as informações do LCD
-      delay(1500);
-      lcd.clear();
-    }
-  //Verifica se o client não está conectado.
-  // if (!client.connected()) {
-  //   //Limpa as informações do Display LCD
-  //   lcd.clear();
-  //   //Mostra na primeira linha da tela LCD que o ESP saiu do Local
-  //   lcd.setCursor(0, 0);
-  //   lcd.print("Esperando cliente");
-  //   //Mostra na segunda linha da tela LCD de que local o ESP saiu do
-  //   lcd.setCursor(0, 1);
-  //   lcd.print("MAC:FC5C45004FC8");
-  // }
+void mensagemClient(){
+  //Verifica se o client(ESP32) mandou alguma mensagem.  
+  if (client.available()) {
+
+  // Lê a mensagem enviada pelo cliente
+  String mensagem = client.readStringUntil('\n');
+
+  //retorna uma nova string sem os espaços em branco.
+  mensagem.trim();
+
+  // Imprime a mensagem recebida do client.
+  Serial.print("Mensagem recebida: ");
+  Serial.println(mensagem);
+
+  //Da um delay de 0.5segundos
+  delay(500);
+
+  lcd.setCursor(0, 0);
+  lcd.print(mensagem);
+  // Posiciona o cursor na segunda linha, primeira coluna e imprime " "
+  delay(5000);
+  lcd.setCursor(0, 1);
+  lcd.print(mensagem);
+
 }
 
-void alertaConexao(int led, char* message) {
+
+
+}
+
+void alertaConexao(int led, char * message) {
   while (true) {
     Serial.println(message);
     digitalWrite(led, HIGH);
@@ -167,102 +122,46 @@ void alertaConexao(int led, char* message) {
   }
 }
 
-// void callback(char* topic, byte* payload, unsigned int length) {
-//   Serial.print("Message arrived [");
-//   Serial.print(topic);
-//   Serial.print("] ");
-//   for (int i = 0; i < length; i++) {
-//     Serial.print((char)payload[i]);
-//   }
-//   Serial.println();
-
-//   // Verificar a distância e enviar mensagem para o broker MQTT se for menor que 5
-//   if (distancCm < 5) {
-//     if (client.connected()) {
-//       String mensagem = "ESP32 saiu!";
-//       // client.publish("seu/topico/aqui", mensagem.c_str());
-//       Serial.println(mensagem);
-//     }
-//   }
-// }
-
 
 void setup() {
-  //Seta o serial que sairá as informações de print.
-  Serial.begin(9600);
+  //Porta de saida para as informações(Porta Serial)
+  Serial.begin(9600); 
 
-  //Define os pinos de saida do Buzzer e do Trig COVAS
-  pinMode(pinoTrig, OUTPUT);
-  pinMode(pinoBuzzer, OUTPUT);
-  //Define o pino de entrada do Som COVAS
-  pinMode(pinoEcho, INPUT);
-
-  //Define os Pinos dos LED's;
+  //definição dos LEDS 
   pinMode(naoConectadoLedVermelho, OUTPUT);
   pinMode(conectandoLedAmarelo, OUTPUT);
   pinMode(conectadoLedVerde, OUTPUT);
 
-  //Chama a função quebrou.
-  quebrou();
-  //Conecta ao Wifi
+  //Conecta com o Wifi pegando o Nome e a Senha da rede
   WiFi.begin(ssid, password);
-  //Chama a função connectWifi
+
+  //Chama a função connectWifi. 
   connectWifi();
-  //Desliga o LED vermelho.
+
+  //Desliga o Led vermelho assim que conecta ao Wifi
   digitalWrite(naoConectadoLedVermelho, LOW);
+
   //Chama a função infoNet.
   infoNet();
+
   //Chama a função iniciaServer.
   iniciaServer();
-  //Chama a função LCD.
+
+  //Chama a função LCD
   LCD();
 
-    // put your setup code here, to run once:
-  //Serial.begin(115200); 
-
-
-  // ubidots.setDebug(true);  // uncomment this to make debug messages available
-  // ubidots.connectToWifi(ssid, password);
-  // // ubidots.setCallback(callback);
-  // ubidots.setup();
-  // ubidots.reconnect();
-  // ubidots.subscribeLastValue(DEVICE_LABEL, VARIABLE_LABEL); 
 }
+
 
 void loop() {
 
-  // if (millis() - timer > PUBLISH_FREQUENCY) {
-  //   ubidots.add(VARIABLE_LABEL, 0);  // Insira variável e dispositivo a serem publicados
-  //   ubidots.publish(DEVICE_LABEL);
-  //   timer = millis();
-  // }
-
-  //Chama a função quebrou.
-  quebrou();
-  //Da um delay de 0.5 segundos.
-  delay(500);
-  //Liga o led verde
-  digitalWrite(conectadoLedVerde, HIGH);
-  //Liga o led Amarelo
-  digitalWrite(conectandoLedAmarelo, HIGH);
+  //Liga o Led Verde
+  digitalWrite(conectadoLedVerde, HIGH); 
   //Chama a função verificaCliente.
   verificaCliente();
-  //Da um delay de 0.5 segundos.
-  mensagemCliente();
-
-  delay(500);
-  
-  if (!client.connected()){
-    Serial.println("ainda falta");
-
-  }
   //Chama a função mensagemCliente.
-  //Da um delay de 0.5 segundos.
-  delay(500);
+  mensagemClient();
+  //Liga o Led amarelo
+  digitalWrite(conectandoLedAmarelo, HIGH);
 
-  // if (!ubidots.connected()) {
-  //   ubidots.reconnect();
-  //   ubidots.subscribeLastValue(DEVICE_LABEL, VARIABLE_LABEL);
-  // }
-  // ubidots.loop();
 }
