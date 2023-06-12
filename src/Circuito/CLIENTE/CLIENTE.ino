f48d6bef7a0671bf7cbb36236936b4461373da2a
Txrmarcos <134190163+MarcosTeixeira0@users.noreply.github.com>
2023-06-12 15:28:12 -0300

    Comentarios

--- src/Circuito/CLIENTE/CLIENTE.ino
+++ src/Circuito/CLIENTE/CLIENTE.ino
@@ -4,98 +4,135 @@
 #define velocidade_som 0.034
 #define polegadas 0.40
 //----------------------------------------------------------------//
-const char* ssid = "Inteli-COLLEGE";
-const char* password = "QazWsx@123";
-const char* serverAddress = "10.128.69.170";  // Endereço IP local do servidor
-int serverPort = 3002;                        // Porta usada pelo servidor
-WiFiClient client;
+const char* ssid = "Inteli-COLLEGE";//Nome da Rede;
+
+const char* senha = "QazWsx@123";//Senha do WiFi da Rede;
+
+const char* enderecoMac = "10.128.69.170"; // Endereço IP local do servidor
+
+int portaServidor = 3002;// Porta usada pelo servidor;
+
+WiFiClient client; //Salva WiFiClient em client;
 //-------------------------------------------------------------------//
 // Número máximo de endereços MAC que podem ser armazenados
-const int MAX_MACS = 5;
-String macs[MAX_MACS];
+const int qntMacs = 5;
+
+String macs[qntMacs];//lista de endereços macs
+
 int numMacs = 0;
-int cont = 0;
-String complete;
-//-------------------------------------------------------------------//
-//Definindo vaiaveis para o sistema de quebra do ESP
-long duracao;
-float distancCm;
-float distanciaInch;
-const int pinoBuzzer = 25;
-const int pinoTrig = 26;
-const int pinoEcho = 27;
-float potencia;
+
+int contador = 0;
+
+String mensagemParaHost;
+
 String sala;
 //-------------------------------------------------------------------//
-void wifiWait() {
+//Definindo vaiaveis para o sistema de quebra do ESP
+
+long duracao;
+
+float distancCm;
+
+float distanciaInch;
+
+const int pinoBuzzer = 25;
+
+const int pinoTrig = 26;
+
+const int pinoEcho = 27;
+
+float potencia;
+
+//-------------------------------------------------------------------//
+
+void aguardaWifi() {
   //Entra num loop se o dispositivo (ESP32) não conecta a rede.
   while (WiFi.status() != WL_CONNECTED) {
     Serial.println("Conectando ao WiFi...");
     delay(1000);
   }
 }
+
 //-------------------------------------------------------------------//
-void infoRede() {
-  //Retorna no Serial que o disponitivo esta conectado e o Indereço IP Local
+void informacaoDaRede() {
+
+  //Retorna no Serial que o disponitivo esta conectado ao Wifi
   Serial.println("Conectado ao WiFi!");
-  Serial.print("Endereço IP: ");
+  
+  Serial.print("Endereço IP: ");//Printa o Endereço IP Local
   Serial.println(WiFi.localIP());
-  String bssidStr = WiFi.BSSIDstr();           // pega o endereço mac em tempo real
-  Serial.print("Endereço MAC do roteador: ");  //printa o endereço MAC
+
+  String bssidStr = WiFi.BSSIDstr(); // Pega o endereço mac em tempo real
+  Serial.print("Endereço MAC do roteador: "); //printa o endereço MAC
   Serial.println(bssidStr);
-  //Coleta o valor da potencia do sinal do WiFi
-  //Funcao para enviar a potencia do sinal
+
+  //Printa a Potencia do WiFi;
+  
   Serial.print("Potencia do sinal: ");
   Serial.println(potencia);
 }
 //-------------------------------------------------------------------//
-void serverConect() {
+
+void conectaServidor() {
   // Se o client não conecta no ip e na porta ele entra num loop que volta no Serial a mensagem : "Falha na conexão com o server."
-  while (!client.connect(serverAddress, serverPort)) {
+  while (!client.connect(enderecoMac, portaServidor)) {
     Serial.println("Falha na conexão com o server.");
     delay(1000);
   }
 }
+
 //-------------------------------------------------------------------//
-String returnMac() {
-  String bssidStr = WiFi.BSSIDstr();  //pega a string do endereço mac
+String returnaMac() {
+  
+  String mensagem = WiFi.BSSIDstr();  //pega a string do endereço mac
   //Salva o endereço MAC na varial mensagem
-  String mensagem = bssidStr;  // acho q o erro ta aqui, o bssidStr não está atualizando quando eu uso o replace
-  mensagem.replace(":", "");
-  //Verifica se o outro dispositivo mandou mensagem.
-  client.available();
+
+  mensagem.replace(":", "");//tira os dois pontos do endereço mac.
+
   // Envia o Endereço MAC e a potencia do sinal para o servidor
   return mensagem;
 }
 
-String fullMsn(String x) {
-  //chama a função identificaLocal() e passa como parametro a string de retorno da função returnMac()
-  float dist = WiFi.RSSI();
-  String complete = x + "#" + dist;
-  Serial.println(complete);
-  client.println(complete);
-  return complete;
+String concatenaMensagem(String x) {
+
+
+  float distancia = WiFi.RSSI(); //Salva na variavel distancia a intesidade do sinal do wifi;
+  
+  String mensagemParaHost = x + "#" + distancia; //Salva na variavel mensagemParaHost = o argumento que é passado pela função + "#" + a intesidade do sinal do wifi;
+  
+  Serial.println(mensagemParaHost); //printa no serial
+  client.println(mensagemParaHost); //manda para o host a variavel mensagemParaHost
+  
+  return mensagemParaHost; //retorna a string mensagemParaHost
 }
 
 //-------------------------------------------------------------------//
 void identificaLocal(String mensagem) {
+
   // Verifica se a mensagem é um endereço MAC válido
+  
   if (mensagem.length() == 12) {
+  
     // Verifica se o endereço MAC já está na lista
     bool encontrado = false;
+
     for (int i = 0; i < numMacs; i++) {
       if (mensagem == macs[i]) {
         encontrado = true;
         break;
       }
     }
-    if (!encontrado) {
-      // Adiciona o endereço MAC à lista
-      if (numMacs < MAX_MACS) {
+
+    if (!encontrado) { //se for diferente de encontrado entra no "if"
+      
+      if (numMacs < qntMacs) { 
+
         macs[numMacs] = mensagem;
-        client.println(macs[numMacs]);  // joga o para o serial do client
-        numMacs++;
-        delay(500);
+
+        client.println(macs[numMacs]);  // joga o para o serial do client o endereço mac que ele está conectado;
+
+        numMacs++;//incrementa a variavel numMacs
+        
         Serial.println("Endereço MAC adicionado à lista.");
       } else {
         Serial.println("A lista de endereços MAC está cheia.");
@@ -106,82 +143,128 @@ void identificaLocal(String mensagem) {
   } else {
     Serial.println("Endereço MAC inválido.");
   }
-  if (mensagem == macs[cont]) {
-    Serial.println("marcos ta no 1");
-    //client.println("marcos ta no 1");
-    sala = fullMsn("1");
-    client.println(sala);
+
+
+
+  if (mensagem == macs[contador]) {
     
+    Serial.println("marcos ta no 1");//Printa no serial
+    
+    sala = concatenaMensagem("1");//guarda na variavel "sala" o resultado da função concatenaMensagem();
+
+    client.println(sala);// Manda para o host a variavel "sala"
   }
-  cont++;
-  if (mensagem == macs[cont]) {
-    Serial.println("marcos ta no 2");
-    //client.println("marcos ta no 2");
-    sala = fullMsn("2");
-    client.println(sala);
+
+  contador++;
+
+  if (mensagem == macs[contador]) {
+    
+    Serial.println("marcos ta no 2");//Printa no serial
+    
+    sala = concatenaMensagem("2");//guarda na variavel "sala" o resultado da função concatenaMensagem();
+
+    client.println(sala);// Manda para o host a variavel "sala"
   }
-  cont++;
-  if (mensagem == macs[cont]) {
-    Serial.println("marcos ta no 3");
-    //client.println("marcos ta no 3");
-    sala = fullMsn("3");
-    client.println(sala);
+
+  contador++;
+
+  if (mensagem == macs[contador]) {
+    
+    Serial.println("marcos ta no 3");//Printa no serial
+    
+    sala = concatenaMensagem("3");//guarda na variavel "sala" o resultado da função concatenaMensagem();
+
+    client.println(sala);// Manda para o host a variavel "sala"
   }
-  cont++;
-  if (mensagem == macs[cont]) {
-    Serial.println("marcos ta no 4");
-    //client.println("marcos ta no 4");
-    sala = fullMsn("4");
-    client.println(sala);
+
+  contador++;
+
+  if (mensagem == macs[contador]) {
+
+    Serial.println("marcos ta no 4"); //printa no serial 
+  
+    sala = concatenaMensagem("4"); //guarda na variavel "sala" o resultado da função concatenaMensagem();
+
+    client.println(sala); // Manda para o host a variavel "sala"
   }
-  cont = 0;
+  
+  contador = 0;
 }
 //-------------------------------------------------------------------//
 
 void quebrou() {
+
   digitalWrite(pinoTrig, LOW);
   delayMicroseconds(2);
+
   digitalWrite(pinoTrig, HIGH);
   delayMicroseconds(10);
+
   digitalWrite(pinoTrig, LOW);
+
   duracao = pulseIn(pinoEcho, HIGH);         // Calcule a distância
   distancCm = duracao * velocidade_som / 2;  // Converter para centimetros
   distanciaInch = distancCm * polegadas;     // Converter para polegadas
-  if (distancCm > 7) {
-    digitalWrite(pinoBuzzer, HIGH);
-    Serial.print("ESP32 retirado do dispositivo!!!");
-    delay(2000);
+  
+  //logica que diz que se a distancia for maior que 7cm o Buzzer irá apitar;
+  if (distancCm > 7) { 
+
+    digitalWrite(pinoBuzzer, HIGH); //ativa o Buzzer
+    Serial.print("ESP32 retirado do dispositivo!!!");//Printa msg no serial
+
+    delay(2000);//da um delay de 2 s
     digitalWrite(pinoBuzzer, LOW);
   }
-  Serial.print("Distancia (cm): ");  // Imprime a distância no Serial Monitor
+
+  Serial.print("Distancia (cm): "); //printa no Serial a distancia em cm;
   Serial.println(distancCm);
-  Serial.print("Distancia (inch): ");
+
+  Serial.print("Distancia (inch): ");//printa no Serial a distancia em Inch;
   Serial.println(distanciaInch);
-  delay(1000);
+
+  delay(1000); // Dá um delay de 1s
 }
+
 //-------------------------------------------------------------------//
+
 void setup() {
-  //define a porta do serial que mostrara as informarções
+  //define a porta do serial que mostrara as informarções;
   Serial.begin(9600);
-  // quebrou();
-  WiFi.begin(ssid, password);
-  //Chama a função wifiWait
-  wifiWait();
-  //Chama a função infoRede
-  infoRede();
-  //Chama a função serverConect
-  serverConect();
+  
+  //Função que faz a conecção com Wifi;
+  WiFi.begin(ssid, senha);
+  
+  //Chama a função aguardaWifi();
+  
+  aguardaWifi();
+  
+  //Chama a função informacaoDaRede();
+  
+  informacaoDaRede();
+
+  //Chama a função conectaServidor();
+
+  conectaServidor();
 }
+
+//-------------------------------------------------------------------//
+
 void loop() {
-  //Funcao para envio de notificacao caso o ESP seja danificado
+
+  //Função para envio de notificacao caso o ESP seja danificado;
   quebrou();
+
   //Mostra no serial que está enviando a mensagem para o servidor.
+
   Serial.println("Enviando Mensagem ao Server:");
-  // Verifica se há dados disponíveis no Serial
-  //returnMac()
-  identificaLocal(returnMac());
-  // fullmsn();
+
+  //chama a função identificaLocal() e passamos como argumento a função returnMac() que retorna uma string;
+
+  identificaLocal(returnaMac());
+
+  // Prita no Serial qual id da Sala ele está;
   Serial.print(sala);
-  //Passa a potencia do Wifi para o host
+
+  //Prita no Serial qual a potencia do Wifi da sala onde ele está ;
   Serial.println(potencia);
 }
