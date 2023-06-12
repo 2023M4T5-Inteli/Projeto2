d3fdaeb250ac459c47c281bc7d7a6012a447b51a
Txrmarcos <134190163+MarcosTeixeira0@users.noreply.github.com>
2023-06-12 14:41:54 -0300

    .

--- src/Circuito/CLIENTE/CLIENTE.ino
+++ src/Circuito/CLIENTE/CLIENTE.ino
@@ -1,30 +1,22 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
-
 //----------------------------------------------------------------//
-
 #define velocidade_som 0.034
 #define polegadas 0.40
-
 //----------------------------------------------------------------//
-
 const char* ssid = "Inteli-COLLEGE";
 const char* password = "QazWsx@123";
-const char* serverAddress = "10.128.71.32";  // Endereço IP local do servidor
-int serverPort = 8000;                       // Porta usada pelo servidor
+const char* serverAddress = "10.128.69.170";  // Endereço IP local do servidor
+int serverPort = 3002;                        // Porta usada pelo servidor
 WiFiClient client;
-
 //-------------------------------------------------------------------//
-
 // Número máximo de endereços MAC que podem ser armazenados
 const int MAX_MACS = 5;
 String macs[MAX_MACS];
 int numMacs = 0;
 int cont = 0;
 String complete;
-
 //-------------------------------------------------------------------//
-
 //Definindo vaiaveis para o sistema de quebra do ESP
 long duracao;
 float distancCm;
@@ -34,11 +26,7 @@ const int pinoTrig = 26;
 const int pinoEcho = 27;
 float potencia;
 String sala;
-const int inicia = 2;
-const int botaoResetCLIENTE = 32;
-
 //-------------------------------------------------------------------//
-
 void wifiWait() {
   //Entra num loop se o dispositivo (ESP32) não conecta a rede.
   while (WiFi.status() != WL_CONNECTED) {
@@ -46,28 +34,21 @@ void wifiWait() {
     delay(1000);
   }
 }
-
 //-------------------------------------------------------------------//
-
-void infRede() {
+void infoRede() {
   //Retorna no Serial que o disponitivo esta conectado e o Indereço IP Local
   Serial.println("Conectado ao WiFi!");
   Serial.print("Endereço IP: ");
   Serial.println(WiFi.localIP());
-
   String bssidStr = WiFi.BSSIDstr();           // pega o endereço mac em tempo real
   Serial.print("Endereço MAC do roteador: ");  //printa o endereço MAC
   Serial.println(bssidStr);
-
   //Coleta o valor da potencia do sinal do WiFi
-  potencia = WiFi.RSSI();
   //Funcao para enviar a potencia do sinal
   Serial.print("Potencia do sinal: ");
   Serial.println(potencia);
 }
-
 //-------------------------------------------------------------------//
-
 void serverConect() {
   // Se o client não conecta no ip e na porta ele entra num loop que volta no Serial a mensagem : "Falha na conexão com o server."
   while (!client.connect(serverAddress, serverPort)) {
@@ -75,36 +56,28 @@ void serverConect() {
     delay(1000);
   }
 }
-
 //-------------------------------------------------------------------//
-
 String returnMac() {
   String bssidStr = WiFi.BSSIDstr();  //pega a string do endereço mac
-
-  //Salva o endereço MAC na varial bssidStr
-
-  bssidStr.replace(":", "");
-
+  //Salva o endereço MAC na varial mensagem
+  String mensagem = bssidStr;  // acho q o erro ta aqui, o bssidStr não está atualizando quando eu uso o replace
+  mensagem.replace(":", "");
   //Verifica se o outro dispositivo mandou mensagem.
   client.available();
-
   // Envia o Endereço MAC e a potencia do sinal para o servidor
-  return bssidStr;
+  return mensagem;
 }
 
-
-//-------------------------------------------------------------------//
-String fullmsn(String x) {
+String fullMsn(String x) {
   //chama a função identificaLocal() e passa como parametro a string de retorno da função returnMac()
-  // String mac = returnMac();
-  // Serial.println(mac);
-  complete = x + "#" + potencia;
+  float dist = WiFi.RSSI();
+  String complete = x + "#" + dist;
   Serial.println(complete);
   client.println(complete);
   return complete;
 }
-//-------------------------------------------------------------------//
 
+//-------------------------------------------------------------------//
 void identificaLocal(String mensagem) {
   // Verifica se a mensagem é um endereço MAC válido
   if (mensagem.length() == 12) {
@@ -116,7 +89,6 @@ void identificaLocal(String mensagem) {
         break;
       }
     }
-
     if (!encontrado) {
       // Adiciona o endereço MAC à lista
       if (numMacs < MAX_MACS) {
@@ -134,191 +106,82 @@ void identificaLocal(String mensagem) {
   } else {
     Serial.println("Endereço MAC inválido.");
   }
-
   if (mensagem == macs[cont]) {
     Serial.println("marcos ta no 1");
-    sala = fullmsn("1");
+    //client.println("marcos ta no 1");
+    sala = fullMsn("1");
     client.println(sala);
+    
   }
   cont++;
   if (mensagem == macs[cont]) {
     Serial.println("marcos ta no 2");
-    sala = fullmsn("2");
+    //client.println("marcos ta no 2");
+    sala = fullMsn("2");
     client.println(sala);
   }
   cont++;
   if (mensagem == macs[cont]) {
     Serial.println("marcos ta no 3");
-    sala = fullmsn("3");
+    //client.println("marcos ta no 3");
+    sala = fullMsn("3");
     client.println(sala);
   }
   cont++;
   if (mensagem == macs[cont]) {
     Serial.println("marcos ta no 4");
-    sala = fullmsn("4");
+    //client.println("marcos ta no 4");
+    sala = fullMsn("4");
     client.println(sala);
   }
   cont = 0;
 }
-
 //-------------------------------------------------------------------//
 
-
-void latitudeWifi(float& latitude, float& longitude) {
-  WiFi.scanNetworks();
-
-  int nRede = WiFi.scanComplete();
-  if (nRede == -2) {
-    WiFi.scanNetworks(true);
-    return;
+void quebrou() {
+  digitalWrite(pinoTrig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pinoTrig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pinoTrig, LOW);
+  duracao = pulseIn(pinoEcho, HIGH);         // Calcule a distância
+  distancCm = duracao * velocidade_som / 2;  // Converter para centimetros
+  distanciaInch = distancCm * polegadas;     // Converter para polegadas
+  if (distancCm > 7) {
+    digitalWrite(pinoBuzzer, HIGH);
+    Serial.print("ESP32 retirado do dispositivo!!!");
+    delay(2000);
+    digitalWrite(pinoBuzzer, LOW);
   }
-
-  if (nRede == 0) {
-    Serial.println("Nenhum ponto de acesso Wi-Fi encontrado");
-    return;
-  }
-
-  int bestSignal = -999;
-  int bestNetwork = -1;
-  for (int i = 0; i < nRede; i++) {
-    int signal = WiFi.RSSI(i);
-    if (signal > bestSignal) {
-      bestSignal = signal;
-      bestNetwork = i;
-    }
-  }
-
-  if (bestNetwork == -1) {
-    Serial.println("Falha ao obter a localização");
-    return;
-  }
-
-  latitude = WiFi.BSSID(bestNetwork)[0];
-  longitude = WiFi.BSSID(bestNetwork)[1];
+  Serial.print("Distancia (cm): ");  // Imprime a distância no Serial Monitor
+  Serial.println(distancCm);
+  Serial.print("Distancia (inch): ");
+  Serial.println(distanciaInch);
+  delay(1000);
 }
-
-
-
-
-
-
-
-
-
-
-
-//-------------------------------------------------------------------//
-
-// void quebrou() {
-//   digitalWrite(pinoTrig, LOW);
-//   delayMicroseconds(2);
-//   digitalWrite(pinoTrig, HIGH);
-//   delayMicroseconds(10);
-//   digitalWrite(pinoTrig, LOW);
-//   duracao = pulseIn(pinoEcho, HIGH);         // Calcule a distância
-//   distancCm = duracao * velocidade_som / 2;  // Converter para centimetros
-//   distanciaInch = distancCm * polegadas;     // Converter para polegadas
-//   if (distancCm > 7) {
-//     digitalWrite(pinoBuzzer, HIGH);
-//     Serial.print("ESP32 retirado do dispositivo!!!");
-//     delay(2000);
-//     digitalWrite(pinoBuzzer, LOW);
-//   }
-//   Serial.print("Distancia (cm): ");  // Imprime a distância no Serial Monitor
-//   Serial.println(distancCm);
-//   Serial.print("Distancia (inch): ");
-//   Serial.println(distanciaInch);
-//   delay(1000);
-// }
-
-//-------------------------------------------------------------------//
-void distanciaCalc(float x) {
-
-  Serial.println(x);
-  if ((x < -10) && (x >= -40)) {
-
-    Serial.println("Zona segura A");
-  } else if ((x < -40) && (x >= -65)) {
-    Serial.println("Zona Segura B");
-  } else if ((x < -65) && (x >= -100)) {
-    Serial.println("Zona Segura C");
-  } else {
-    Serial.println("Fora da Zona");
-  }
-}
-
-
 //-------------------------------------------------------------------//
 void setup() {
   //define a porta do serial que mostrara as informarções
   Serial.begin(9600);
   // quebrou();
-
   WiFi.begin(ssid, password);
   //Chama a função wifiWait
   wifiWait();
-  //Chama a função infRede
-  infRede();
+  //Chama a função infoRede
+  infoRede();
   //Chama a função serverConect
-  //serverConect();
-  pinMode(inicia, INPUT_PULLUP);
-
-
-
-
-  // Obter a latitude e a longitude aproximadas
-  // float latitude, longitude;
-  // latitudeWifi(latitude, longitude);
-
-  // Serial.print("Latitude: ");
-  // Serial.println(latitude, 6);
-
-  // Serial.print("Longitude: ");
-  // Serial.println(longitude, 6);
+  serverConect();
 }
-
-
 void loop() {
-
-  client.println("Covas");
-  Serial.println(".");
-  client.println("Covas");
-
-  client.println("marcos");
-
-  float nivelR = WiFi.RSSI();
   //Funcao para envio de notificacao caso o ESP seja danificado
-  //  quebrou();
-
+  quebrou();
   //Mostra no serial que está enviando a mensagem para o servidor.
-  // Serial.println("Enviando Mensagem ao Server:");
-
-  float latitude, longitude;
-  latitudeWifi(latitude, longitude);
-
-  Serial.print("Latitude: ");
-  Serial.println(latitude, 6);
-
-  Serial.print("Longitude: ");
-  Serial.println(longitude, 6);
-  distanciaCalc(nivelR);
-
-
-  //Verifica se há dados disponíveis no Serial
-  String x = returnMac();
-
-  identificaLocal(x);
-
+  Serial.println("Enviando Mensagem ao Server:");
+  // Verifica se há dados disponíveis no Serial
+  //returnMac()
+  identificaLocal(returnMac());
+  // fullmsn();
   Serial.print(sala);
   //Passa a potencia do Wifi para o host
-  //erial.println(potencia);
-
-// Verifica se o botão de reset do CLIENTE foi pressionado
-  if (digitalRead(botaoResetCLIENTE) == LOW) {
-    Serial.println("Reiniciou");
-  // Reinicia o ESP32
-    ESP.restart();
+  Serial.println(potencia);
 }
-}
-
-
